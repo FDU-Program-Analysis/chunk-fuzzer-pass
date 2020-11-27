ea292226d4c92eb9fefd51f47fcc60ded972e843
Dr-XX <jordan0801@163.com>
2020-11-27 19:00:57 +0800

    add test files for dfsan_rt

--- test/dfsan_test.c
+++ test/dfsan_test.c
@@ -1,9 +1,13 @@
 /*
 clang -I ../include -fsanitize=dataflow -Xclang -load -Xclang ../install/pass/libDFSanPass.so dfsan_test.c
+
+clang -I /home/jordan/develop/chunk-fuzzer-pass/dfsan_rt/ -I /home/jordan/develop/chunk-fuzzer-pass/include/ -L /home/jordan/develop/chunk-fuzzer-pass/install/lib/ dfsan_test.c -ldfsan_rt-x86_64 -lpthread -lrt -lstdc++ -ldl
+
+
 */
 
-//#include <sanitizer/dfsan_interface.h>
-#include "../dfsan_rt/dfsan_interface.h"
+#include <sanitizer/dfsan_interface.h>
+//#include "../dfsan_rt/dfsan_interface.h"
 #include <assert.h>
 #include <stdio.h>
 
@@ -20,22 +24,22 @@ int main(void) {
   dfsan_label k_label = dfsan_create_label("k", 0);
   dfsan_set_label(k_label, &k, sizeof(k));
 
-  printf("%d",dfsan_get_label_info(i_label));
+  // printf("%d",*dfsan_get_label_info(i_label));
 
-  printf("%d",dfsan_get_label_info(j_label));
+  // printf("%d",*dfsan_get_label_info(j_label));
 
-  printf("%d",dfsan_get_label_info(k_label));
+  // printf("%d",*dfsan_get_label_info(k_label));
 
 
-  // dfsan_label ij_label = dfsan_get_label(i + j);
-  // assert(dfsan_has_label(ij_label, i_label));
-  // assert(dfsan_has_label(ij_label, j_label));
-  // assert(!dfsan_has_label(ij_label, k_label));
+  dfsan_label ij_label = dfsan_get_label(i + j);
+  assert(dfsan_has_label(ij_label, i_label));
+  assert(dfsan_has_label(ij_label, j_label));
+  assert(dfsan_has_label(ij_label, k_label));
 
-  // dfsan_label ijk_label = dfsan_get_label(i + j + k);
-  // assert(dfsan_has_label(ijk_label, i_label));
-  // assert(dfsan_has_label(ijk_label, j_label));
-  // assert(dfsan_has_label(ijk_label, k_label));
+  dfsan_label ijk_label = dfsan_get_label(i + j + k);
+  assert(dfsan_has_label(ijk_label, i_label));
+  assert(dfsan_has_label(ijk_label, j_label));
+  assert(dfsan_has_label(ijk_label, k_label));
 
   return 0;
 }
