43be73a695e61d79bda1b264967ee95f99fb61da
Dr-XX <jordan0801@163.com>
2020-12-02 18:55:53 +0800

    fix bug in dfsan_rt test.

--- test/dump_labels.c
+++ test/dump_labels.c
@@ -5,39 +5,36 @@
 
 // Tests that labels are properly dumped at program termination.
 
-// clang -fsanitize=dataflow dump_labels.c
 
-// angora's args
-// clang /home/jordan/develop/chunk-fuzzer-pass/test/loopTest.bc -o loopTest.tt -Xclang -load -Xclang ./pass/libUnfoldBranchPass.so -Xclang -load -Xclang ./pass/libAngoraPass.so -mllvm -TrackMode -mllvm -angora-dfsan-abilist=./rules/angora_abilist.txt -mllvm -angora-dfsan-abilist=./rules/dfsan_abilist.txt -mllvm -angora-exploitation-list=./rules/exploitation_list.txt -Xclang -load -Xclang ./pass/libDFSanPass.so -mllvm -angora-dfsan-abilist2=./rules/angora_abilist.txt -mllvm -angora-dfsan-abilist2=./rules/dfsan_abilist.txt -pie -fpic -Qunused-arguments -g -O3 -funroll-loops -Wl,--whole-archive ./lib/libdfsan_rt-x86_64.a -Wl,--no-whole-archive -Wl,--dynamic-list=./lib/libdfsan_rt-x86_64.a.syms ./lib/libruntime.a ./lib/libDFSanIO.a -lstdc++ -lrt -Wl,--no-as-needed -Wl,--gc-sections -ldl -lpthread -lm 
- 
+//NOTE : cannot modify the parameter order
+
 //clang -I /home/jordan/develop/chunk-fuzzer-pass/dfsan_rt/ \
 -I /home/jordan/develop/chunk-fuzzer-pass/include/ \
--Xclang -load -Xclang ./pass/libDFSanPass.so \
--mllvm -chunk-dfsan-abilist=./rules/angora_abilist.txt \
--mllvm -chunk-dfsan-abilist=./rules/dfsan_abilist.txt \
--pie -fpic -Qunused-arguments -g -O3 -funroll-loops \
--Wl,--whole-archive ./lib/libdfsan_rt-x86_64.a -Wl,--no-whole-archive \
--Wl,--dynamic-list=./lib/libdfsan_rt-x86_64.a.syms \
--lstdc++ -lrt -Wl,--no-as-needed -Wl,--gc-sections \
--ldl -lpthread -lm \
-/home/jordan/develop/chunk-fuzzer-pass/test/dump_labels.c -o dump_labels
+/home/jordan/develop/chunk-fuzzer-pass/test/dump_labels.c -o dump_labels -pie -fpic \
+-Wl,--whole-archive /home/jordan/develop/chunk-fuzzer-pass/install/lib/libdfsan_rt-x86_64.a -Wl,--no-whole-archive \
+-Wl,--dynamic-list=/home/jordan/develop/chunk-fuzzer-pass/install/lib/libdfsan_rt-x86_64.a.syms \
+/home/jordan/develop/chunk-fuzzer-pass/install/lib/libruntime.a \
+-Xclang -load -Xclang /home/jordan/develop/chunk-fuzzer-pass/install/pass/libDFSanPass.so \
+-mllvm -chunk-dfsan-abilist=/home/jordan/develop/chunk-fuzzer-pass/install/rules/angora_abilist.txt \
+-mllvm -chunk-dfsan-abilist=/home/jordan/develop/chunk-fuzzer-pass/install/rules/dfsan_abilist.txt \
+-lstdc++ -ldl -lpthread -lm -g 
 
-//#include "../dfsan_rt/dfsan_interface.h"
-#include <sanitizer/dfsan_interface.h>
+#include "../dfsan_rt/dfsan_interface.h"
+//#include <sanitizer/dfsan_interface.h>
 #include <assert.h>
 #include <stdio.h>
 
 int main(int argc, char** argv) {
   int i = 1;
-  dfsan_label i_label = dfsan_create_label("i", 0);
+  dfsan_label i_label = dfsan_create_label(0);
   dfsan_set_label(i_label, &i, sizeof(i));
 
   int j = 2;
-  dfsan_label j_label = dfsan_create_label("j", 0);
+  dfsan_label j_label = dfsan_create_label(2);
   dfsan_set_label(j_label, &j, sizeof(j));
 
   int k = 3;
-  dfsan_label k_label = dfsan_create_label("k", 0);
+  dfsan_label k_label = dfsan_create_label(3);
   dfsan_set_label(k_label, &k, sizeof(k));
 
   dfsan_label ij_label = dfsan_get_label(i + j);
@@ -56,7 +53,7 @@ int main(int argc, char** argv) {
     // Exhaust the labels.
     unsigned long num_labels = 1 << (sizeof(dfsan_label) * 8);
     for (unsigned long i =  ijk_label + 1; i < num_labels - 2; ++i) {
-      dfsan_label l = dfsan_create_label("l", 0);
+      dfsan_label l = dfsan_create_label(0);
       assert(l == i);
     }
 
@@ -67,7 +64,7 @@ int main(int argc, char** argv) {
     // Try to allocate another label (either explicitly or by unioning two
     // existing labels), but expect a crash.
     if (argv[1][0] == 'c') {
-      l = dfsan_create_label("l", 0);
+      l = dfsan_create_label(0);
     } else {
       l = dfsan_union(6, 7);
     }
