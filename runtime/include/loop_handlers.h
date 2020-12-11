4947c23068fb77ceab9f27a2086c49e539a3b749
Dr-XX <jordan0801@163.com>
2020-12-11 16:43:14 +0800

    update test files

---
+++ runtime/include/loop_handlers.h
@@ -0,0 +1,14 @@
+#ifndef LOOP_HANDLERS_H
+#define LOOP_HANDLERS_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void __chunk_get_dump_label(void *addr, size_t size);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
