f729063d75e66deb2986510563048851d1006871
Dr-XX <jordan0801@163.com>
2021-02-04 15:23:26 +0800

    add new interface in loop_handlers_wrap.rs, save sth in logger

--- common/src/log_data.rs
+++ common/src/log_data.rs
@@ -1,11 +1,12 @@
-use crate::{cond_stmt_base::CondStmtBase, tag::TagSeg};
+use crate::{cond_stmt_base::CondStmtBase, tag::*};
 use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct LogData {
     pub cond_list: Vec<CondStmtBase>,
-    pub tags: HashMap<u32, Vec<TagSeg>>,
+    // pub tags: HashMap<u32, Vec<TagSeg>>,
+    pub tags: HashMap<u64, bool>,
     pub magic_bytes: HashMap<usize, (Vec<u8>, Vec<u8>)>,
 }
 
