5915455f9df366bd16cc6519f34bd7276d9f4339
Dr-XX <jordan0801@163.com>
2021-03-31 19:41:17 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
-PREFIX=`pwd`/install
+#!/bin/bash
+
+BIN_PATH=$(readlink -f "$0")
+ROOT_DIR=$(dirname $BIN_PATH)
+
+set -euxo pipefail
+
+if ! [ -x "$(command -v llvm-config)"  ]; then
+    echo "Please install clang-10"
+    exit 1
+fi
+
+PREFIX=${PREFIX:-${ROOT_DIR}/install/}
 
 cargo build
 cargo build --release
